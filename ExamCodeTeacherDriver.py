1c7a67d1daf4f7998989c49b342c9009c0caa16d
Logan <lfrench15@christs.school>
2020-12-15 12:38:33 +0000

    aded exemplay teacher works

---
+++ ExamCodeTeacherDriver.py
@@ -0,0 +1,19 @@
+import bird
+
+aDuck = bird.Duck("Bob")
+for i in range(0,4):
+    aDuck.growOld()
+print(aDuck.makeSound())
+print(aDuck.__str__())
+
+aCanary = bird.Canary("TweetiePie")
+for i in range(0,10):
+    aCanary.growOld()
+print(aCanary.makeSound())
+print(aCanary.__str__())
+
+aCrow = bird.Crow("Evil Bob")
+for i in range(0,10):
+    aCrow.growOld()
+print(aCrow.makeSound())
+print(aCrow.__str__())
